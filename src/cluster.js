69fc60c0a2eee7b36dff4428cb53676aa4bc0756
Nathan Ziarek <nathanziarek@me.com>
2016-01-07 06:57:38 -0600

    initial clustering thoughts

---
+++ src/cluster.js
@@ -0,0 +1,7 @@
+exports.rate = function (pixelCounts, _callback) {
+    for( key in pixelCounts ){
+        // build array of ab colors
+        // build a mao of ab colors back to rgb colors
+    }
+    _callback(returnObj);
+}
