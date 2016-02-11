e213986b4cb78d285c30f0139070a5f3f4cdc5b0
Nate Ziarek <nziarek@directs.com>
2016-02-11 09:56:03 -0600

    moving machines

--- utilities/swatch-html-builder.js
+++ utilities/swatch-html-builder.js
@@ -1,4 +1,4 @@
-var swatch = require('./src/swatches.js').swatch,
+var swatch = require('../src/swatches.js').swatch,
     fs = require('fs');
 
 
@@ -8,12 +8,12 @@ data = JSON.parse(data);
 
 events = data.apiResults[0].league.season.eventType[0].events;
 
-fs.writeFile("samples/test-swatch.html", "<style>.group{width: 160px; height: 120px;position: relative; float: left; border: 1px #eee solid;margin: 20px;}.swatch{width: 30px; height: 30px; position: absolute; top: 10px; right: 10px}.swatch+.swatch{top: 45px}.swatch+.swatch+.swatch{top: 80px}img{ width: 100px; height: 100px; position: absolute; top:10px; left: 10px }</style>");
+fs.writeFile("samples/test-swatch.html", "<style>.group{width: 160px; height: 120px;position: relative; float: left; border: 1px #eee solid;margin: 20px;}img{ width: 100px; height: 100px; position: absolute; top:10px; left: 10px }</style>");
 
 for (var i = 0; i < events.length; i++) {
 
     var abbrev = (events[i].teams[0].abbreviation);
-    //var abbrev = 'tenn';
+    //var abbrev = 'kansas';
 
     swatchy(abbrev);
 
@@ -22,8 +22,8 @@ for (var i = 0; i < events.length; i++) {
 function swatchy(abbrev) {
 
     var image = './images/' + abbrev + '_300.png';
-    
-    swatch(image, 3, writeToHTML);
+
+    swatch(image, 5, writeToHTML);
 
     function writeToHTML(a) {
         var output = '<div class="group"><img src=".' + image + '" />';
@@ -31,7 +31,7 @@ function swatchy(abbrev) {
         for (var i = 0; i < a.swatch.length; i++) {
             output += '<div class="swatch" style="background: rgb(' + a.swatch[i].r + ',' + a.swatch[i].g + ',' + a.swatch[i].b + ');"></div>'
         }
-        output += "</div>"
+        output += "</div>";
         fs.appendFileSync("samples/test-swatch.html", output);
     }
 }
